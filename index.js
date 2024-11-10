462d6470d33cfa5b8f87b5d2325d8544cb84c9e1
paka04sep <pakawat.20074@gmail.com>
2024-11-10 13:15:58 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,72 +1,45 @@
-for (var i = 0 ; i < document.querySelectorAll(".drum").length ; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-      //alert("ปุ่มถูกกด");
+function playSound(key) {
+  var audio;
+  switch (key) {
+    case 'w':
+      audio = new Audio('sounds/tom-1.mp3');
+      break;
+    case 'a':
+      audio = new Audio('sounds/tom-2.mp3');
+      break;
+    case 's':
+      audio = new Audio('sounds/tom-3.mp3');
+      break;
+    case 'd':
+      audio = new Audio('sounds/tom-4.mp3');
+      break;
+    case 'j':
+      audio = new Audio('sounds/crash.mp3');
+      break;
+    case 'k':
+      audio = new Audio('sounds/kick-bass.mp3');
+      break;
+    case 'l':
+      audio = new Audio('sounds/snare.mp3');
+      break;
+    default:
+      return;   
+  }
+  audio.play();   
+}
 
-      //console.log(this.innerHTML);
-      var b = this.innerHTML
+ 
+document.querySelectorAll(".drum").forEach(function(button) {
+  button.addEventListener("click", function() {
+    var buttonKey = this.innerHTML;   
+    playSound(buttonKey);   
+  });
+});
 
-
-      if (b == 'w'){
-      var audio = new Audio('sounds/tom-1.mp3');
-      audio.play();
-      }else if(b == 'a'){
-      var audio = new Audio('sounds/tom-2.mp3');
-      audio.play();
-      }else if(b == 's'){
-        var audio = new Audio('sounds/tom-3.mp3');
-        audio.play();
-      }else if(b == 'd'){
-        var audio = new Audio('sounds/tom-4.mp3');
-        audio.play();
-      }else if(b == 'j'){
-        var audio = new Audio('sounds/crash.mp3');
-        audio.play();
-      }else if(b == 'k'){
-        var audio = new Audio('sounds/kick-bass.mp3');
-        audio.play();
-      }else if(b == 'l'){
-        var audio = new Audio('sounds/snare.mp3');
-        audio.play();
-      }
-    
-      document.addEventListener("keydown", function(event) {
-        var keyPressed = event.key;  
-        playSound(keyPressed);
-      });
-      
-      function playSound(key) {
-        var audio;
-        switch (key) {
-          case 'w':
-            audio = new Audio('sounds/tom-1.mp3');
-            break;
-          case 'a':
-            audio = new Audio('sounds/tom-2.mp3');
-            break;
-          case 's':
-            audio = new Audio('sounds/tom-3.mp3');
-            break;
-          case 'd':
-            audio = new Audio('sounds/tom-4.mp3');
-            break;
-          case 'j':
-            audio = new Audio('sounds/crash.mp3');
-            break;
-          case 'k':
-            audio = new Audio('sounds/kick-bass.mp3');
-            break;
-          case 'l':
-            audio = new Audio('sounds/snare.mp3');
-            break;
-          default:
-            return;  
-        }
-        audio.play();   
-      }
-
-
-    });
-
+document.addEventListener("keydown", function(event) {
+  var keyPressed = event.key;   
+  playSound(keyPressed);   
+});
 
    
     var student2 = {
@@ -77,7 +50,7 @@ for (var i = 0 ; i < document.querySelectorAll(".drum").length ; i++) {
         id: 121231 , 
     }
     //console.log(student2.languages[1])
-    console.log(JSON.stringify(student2))   
+    console.log(JSON.stringify(student2.languages[1]))   
 
 
 
@@ -95,5 +68,4 @@ for (var i = 0 ; i < document.querySelectorAll(".drum").length ; i++) {
 
 
 
-  }
-
+   
